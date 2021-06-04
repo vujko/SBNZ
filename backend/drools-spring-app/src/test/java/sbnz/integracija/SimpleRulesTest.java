e912f6efe43ed850b28b84af4b0a75308cf0aff2
Vukasin <vukasinj98@gmail.com>
2021-06-04 13:07:58 +0200

    Done more unit testing

--- backend/drools-spring-app/src/test/java/sbnz/integracija/SimpleRulesTest.java
+++ backend/drools-spring-app/src/test/java/sbnz/integracija/SimpleRulesTest.java
@@ -34,20 +34,104 @@ public class SimpleRulesTest {
 		KieContainer kContainer = ks
 				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
         kSession =  kContainer.newKieSession();
-        tag = new Tag(null, TagType.GENRE, "fps");
-        game = new Game(null, "Call Of Duty 2", "Activision", "Activision", new HashSet<Rating>(), new HashSet<Tag>(Arrays.asList(tag)), (float)20.0, "image1", (float)0.0, 0, 0, 0);
+        game = new Game(null, "Call Of Duty 2", "Activision", "Activision", new HashSet<Rating>(), new HashSet<Tag>(), (float)20.0, "image1", (float)0.0, 0, 0, 0);
         userInput = new RecommendDto("fps", 10.0f, 25.0f, "PC", "War", "Multiplayer", "Early access");
+        kSession.setGlobal("userInput", userInput);
         
 	}
 	
 	@Test
-	public void testGenre() {
+	public void testGenreInput() {
+        tag = new Tag(null, TagType.GENRE, "fps");
+		game.setTags(new HashSet<Tag>(Arrays.asList(tag)));
+		
 		kSession.insert(game);
-		kSession.setGlobal("userInput", userInput);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	@Test
+	public void testPriceInput() {
+		//set price out of range
+		game.setPrice(50);
+		kSession.insert(game);
 		
 		int num = kSession.fireAllRules();
 		
 		assertEquals(1, num);
+	}
+	
+	@Test
+	public void testThemeInput() {
+        tag = new Tag(null, TagType.THEME, "War");
+		game.setTags(new HashSet<Tag>(Arrays.asList(tag)));
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	@Test
+	public void testSpecialSectionInput() {
+        tag = new Tag(null, TagType.SPECIAL_SECTION, "Early access");
+		game.setTags(new HashSet<Tag>(Arrays.asList(tag)));
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	@Test
+	public void testPlatformInput() {
+        tag = new Tag(null, TagType.PLATFORM, "PC");
+		game.setTags(new HashSet<Tag>(Arrays.asList(tag)));
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	@Test
+	public void testPlayerSupportInput() {
+        tag = new Tag(null, TagType.PLAYER_SUPPORT, "Multiplayer");
+		game.setTags(new HashSet<Tag>(Arrays.asList(tag)));
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	@Test
+	public void testPopularityRule() {
+        game.setNumOfDownloads(101);
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
+	}
+	
+	
+	@Test
+	public void testHighRatedRule() {
+        game.setAverageRating(5f);
+		
+		kSession.insert(game);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertEquals(10, game.getScore());
 	}
 
 }
