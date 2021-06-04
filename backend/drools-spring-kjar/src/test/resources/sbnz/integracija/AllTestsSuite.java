6c9c82cd0eee35525a6804c2c4812a1686106bb0
Vukasin <vukasinj98@gmail.com>
2021-06-04 02:45:52 +0200

    Started unit testing

---
+++ backend/drools-spring-kjar/src/test/resources/sbnz/integracija/AllTestsSuite.java
@@ -0,0 +1,13 @@
+package sbnz.integracija;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	SimpleRulesTest.class
+})
+public class AllTestsSuite {
+
+}
