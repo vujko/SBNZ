df028398017a9665612d4c0bfbc9adedad618a75
Vukasin <vukasinj98@gmail.com>
2021-06-18 23:01:24 +0200

    123

--- backend/drools-spring-app/src/test/java/sbnz/integracija/ComplexRulesTest.java
+++ backend/drools-spring-app/src/test/java/sbnz/integracija/ComplexRulesTest.java
@@ -14,8 +14,8 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
-import sbnz.integracija.example.dto.RecommendDto;
 import sbnz.integracija.example.facts.*;
+import sbnz.integracija.example.facts.Tag.TagType;
 
 public class ComplexRulesTest {
 	
@@ -30,62 +30,62 @@ public class ComplexRulesTest {
         kSession =  kContainer.newKieSession("rulesSession");      
 	}
 	
-//	@Test
-//	public void testSimilarUserRule() {
-//		Tag tag1 = new Tag(null, TagType.GENRE, "fps");
-//        Tag tag2 = new Tag(null, TagType.THEME, "war");
-//        Tag tag3 = new Tag(null, TagType.PLATFORM, "PC");
-//        Set<Tag> tags = new HashSet<>(Arrays.asList(
-//        		tag1,
-//        		tag2,
-//        		tag3,
-//        		new Tag(null, TagType.SPECIAL_SECTION, "early access"),
-//        		new Tag(null, TagType.PLAYER_SUPPORT, "Multiplayer")));
-//        RegisteredUser tempUser = new RegisteredUser(null, "a@gmail.com", "pass", "first", "las", null);
-//        tempUser.setTags(tags);
-//        kSession.setGlobal("tempUser", tempUser);
-//        
-//        tags = new HashSet<>(Arrays.asList(
-//        		tag1,
-//        		tag2,
-//        		tag3));
-//        RegisteredUser simUser = new RegisteredUser(null, "b@gmail.com", "pass", "first", "las", null);
-//        simUser.setTags(tags);
-//        kSession.insert(simUser);
-//        
-//		int num = kSession.fireAllRules();
-//		
-//		assertEquals(1, num);
-//		assertTrue(simUser.isSimilar());
-//	}
-//	
-//	@Test
-//	public void testSimilarUserRuleFail() {
-//		Tag tag1 = new Tag(null, TagType.GENRE, "fps");
-//        Tag tag2 = new Tag(null, TagType.THEME, "war");
-//        Tag tag3 = new Tag(null, TagType.PLATFORM, "PC");
-//        Set<Tag> tags = new HashSet<>(Arrays.asList(
-//        		tag1,
-//        		tag2,
-//        		tag3,
-//        		new Tag(null, TagType.SPECIAL_SECTION, "early access"),
-//        		new Tag(null, TagType.PLAYER_SUPPORT, "Multiplayer")));
-//        RegisteredUser tempUser = new RegisteredUser(null, "a@gmail.com", "pass", "first", "las", null);
-//        tempUser.setTags(tags);
-//        kSession.setGlobal("tempUser", tempUser);
-//        
-//        tags = new HashSet<>(Arrays.asList(tag1));
-//        RegisteredUser simUser = new RegisteredUser(null, "b@gmail.com", "pass", "first", "las", null);
-//        simUser.setTags(tags);
-//        kSession.insert(simUser);
-//		tags = new HashSet<>(Arrays.asList(tag1));
-//		simUser.setTags(tags);
-//		int num = kSession.fireAllRules();
-//		
-//		assertEquals(0, num);
-//		assertFalse(simUser.isSimilar());
-//	}
-//	
+	@Test
+	public void testSimilarUserRule() {
+		Tag tag1 = new Tag(null, TagType.GENRE, "fps");
+        Tag tag2 = new Tag(null, TagType.THEME, "war");
+        Tag tag3 = new Tag(null, TagType.PLATFORM, "PC");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(
+        		tag1,
+        		tag2,
+        		tag3,
+        		new Tag(null, TagType.SPECIAL_SECTION, "early access"),
+        		new Tag(null, TagType.PLAYER_SUPPORT, "Multiplayer")));
+        RegisteredUser tempUser = new RegisteredUser(null, "a@gmail.com", "pass", "first", "las", null);
+        tempUser.setTags(tags);
+        kSession.setGlobal("tempUser", tempUser);
+        
+        tags = new HashSet<>(Arrays.asList(
+        		tag1,
+        		tag2,
+        		tag3));
+        RegisteredUser simUser = new RegisteredUser(null, "b@gmail.com", "pass", "first", "las", null);
+        simUser.setTags(tags);
+        kSession.insert(simUser);
+        
+		int num = kSession.fireAllRules();
+		
+		assertEquals(1, num);
+		assertTrue(simUser.isSimilar());
+	}
+	
+	@Test
+	public void testSimilarUserRuleFail() {
+		Tag tag1 = new Tag(null, TagType.GENRE, "fps");
+        Tag tag2 = new Tag(null, TagType.THEME, "war");
+        Tag tag3 = new Tag(null, TagType.PLATFORM, "PC");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(
+        		tag1,
+        		tag2,
+        		tag3,
+        		new Tag(null, TagType.SPECIAL_SECTION, "early access"),
+        		new Tag(null, TagType.PLAYER_SUPPORT, "Multiplayer")));
+        RegisteredUser tempUser = new RegisteredUser(null, "a@gmail.com", "pass", "first", "las", null);
+        tempUser.setTags(tags);
+        kSession.setGlobal("tempUser", tempUser);
+        
+        tags = new HashSet<>(Arrays.asList(tag1));
+        RegisteredUser simUser = new RegisteredUser(null, "b@gmail.com", "pass", "first", "las", null);
+        simUser.setTags(tags);
+        kSession.insert(simUser);
+		tags = new HashSet<>(Arrays.asList(tag1));
+		simUser.setTags(tags);
+		int num = kSession.fireAllRules();
+		
+		assertEquals(0, num);
+		assertFalse(simUser.isSimilar());
+	}
+	
 //	@Test
 //	public void testSimilarGamesRule() {
 //		RegisteredUser tempUser = new RegisteredUser(null, "a@gmail.com", "pass", "first", "las", null);
